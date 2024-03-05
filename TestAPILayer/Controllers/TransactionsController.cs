116e7993056e1be4c1f7af24d68ec1de9a5e0b0b
Dimitrios Zervas <dimitri_zervas@hotmail.com>
2024-03-05 18:22:42 +0200

    Added GenerateKeys function

--- TestAPILayer/Controllers/TransactionsController.cs
+++ TestAPILayer/Controllers/TransactionsController.cs
@@ -4,12 +4,32 @@ using PeterO.Cbor;
 using Newtonsoft.Json;
 using System.Security.Cryptography;
 
+
 namespace TestAPILayer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class TransactionsController : ControllerBase
     {
+        private enum KeyType {
+            SIGN,
+            ENCRYPT
+        }
+
+        private const string SECRET_STRING = "SECRET";
+        private const string ENCRYPT_STRING = "ENCRYPT";
+        private const string SIGN_STRING = "SIGN";
+        private const string ENCRYPTS_STRING = "ENCRYPTS";
+        private const string SIGNS_STRING = "SIGNS";
+
+        private static byte[] SECRET;
+        private static byte[] SRC;
+        private static byte[] ENCRYPT;
+        private static byte[] SIGN;
+
+        private static List<byte[]> ENCRYPTS;
+        private static List<byte[]> SIGNS;
+
         // Class that represents a JSON Array of strings used to map a JSON array of strings
         // CBOR arrives as string arrays from frontend and we use this object to
         // map the CBOR JSON arrays.
@@ -29,6 +49,52 @@ namespace TestAPILayer.Controllers
                 return cbor;
             }
         } 
+
+        private static List<byte []> GenerateNKeys(int n, byte [] SRC, KeyType type, byte [] baseKey)
+        {
+            List<byte[]> keys = new List<byte[]>();
+            byte[] salt, info;
+
+            if (type == KeyType.SIGN)
+            {
+                salt = SRC; // salt needed to generate keys
+                info = Encoding.UTF8.GetBytes(SIGNS_STRING + n);
+            }
+            else
+            {
+               
+                salt = SRC; // salt needed to generate keys
+                info = Encoding.UTF8.GetBytes(ENCRYPTS_STRING + n);                
+            }
+
+            for (int i = 0; i < n; i++)
+            {              
+                byte[] key = HKDF.DeriveKey(HashAlgorithmName.SHA256, baseKey, 32, salt, info);
+                keys.Add(key);
+            }
+
+            return keys;
+        }
+
+        // Genarates keys in the same way as the JavaScript
+        // protocol.js module but the key that are generated
+        // are not matching the JavaScript version
+        private static void GenerateKeys(int n)
+        {
+            byte[] secretBytes = Encoding.UTF8.GetBytes(SECRET_STRING);
+            //SECRET = HKDF.Extract(HashAlgorithmName.SHA256, secretBytes);
+            SECRET = HKDF.DeriveKey(HashAlgorithmName.SHA256, secretBytes, secretBytes.Length);
+            Console.WriteLine(SECRET.Length);
+            SRC = HKDF.DeriveKey(HashAlgorithmName.SHA256, SECRET, 8, null, null);
+            Console.WriteLine(SRC.Length);
+            Console.WriteLine($"API Layer's SRC: {Encoding.UTF8.GetString(SRC)}");
+            ENCRYPT = HKDF.DeriveKey(HashAlgorithmName.SHA256, SECRET, 32, SRC, Encoding.UTF8.GetBytes(ENCRYPT_STRING));
+            //ENCRYPT = HKDF.DeriveKey(HashAlgorithmName.SHA256, ENCRYPT, 32, SRC, Encoding.UTF8.GetBytes(ENCRYPT_STRING));
+            SIGN = HKDF.DeriveKey(HashAlgorithmName.SHA256, SECRET, 32, SRC, Encoding.UTF8.GetBytes(SIGN_STRING));
+
+            ENCRYPTS = GenerateNKeys(n, SRC, KeyType.ENCRYPT, ENCRYPT);
+            SIGNS = GenerateNKeys(n, SRC, KeyType.SIGN, SIGN);
+        }
 
         // Converts a byte string to a Base64 string
         private static string ConvertStringToBase64(string encoded)
@@ -89,7 +155,7 @@ namespace TestAPILayer.Controllers
             return Convert.FromBase64String(base64String);
         }
 
-        public static byte[] AESDecrypt(byte[] encryptedBytes, byte[] aesKey)
+        public static byte[] AESDecrypt(byte[] encryptedBytes, byte[] aesKey, byte[] iv)
         {
 
             byte[] decryptedBytes;
@@ -102,6 +168,7 @@ namespace TestAPILayer.Controllers
                 {
                     aes.Key = aesKey;
                     aes.IV = new byte[16];
+                    Array.Copy(iv, aes.IV, iv.Length);
 
                     // Create an decryptor to perform the stream transform.
                     ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
@@ -135,17 +202,20 @@ namespace TestAPILayer.Controllers
             var stringArray = JSONArrayToList(jsonArrayString);
             
             byte [] src = StringToBytes(stringArray[stringArray.Count - 1]);
-            Console.WriteLine($"SRC: {Encoding.UTF8.GetString(src)}");
+            Console.WriteLine($"Received SRC from test-ui: {Encoding.UTF8.GetString(src)}");
             Console.WriteLine();
 
             // allocate memory for the data shards byte matrix
             // Last element in the string array is not a shard but the SRC array 
             int numShards = stringArray.Count - 1;
+            GenerateKeys(numShards);
+            
             byte[][] dataShards = new byte[numShards][];
             for (int i = 0; i < numShards; i++)
             {
                 // convert string to bytes
-                byte[] shardBytes = StringToBytes(stringArray[i]);
+                byte[] encryptedShardBytes = StringToBytes(stringArray[i]);
+                byte[] shardBytes = AESDecrypt(encryptedShardBytes, ENCRYPTS[i], SRC);
 
                 // Write to console out for debug
                 Console.WriteLine($"shard[{i}]: {Encoding.UTF8.GetString(shardBytes)}");
@@ -154,7 +224,7 @@ namespace TestAPILayer.Controllers
                 dataShards[i] = new byte [shardBytes.Length];
                 Array.Copy(shardBytes, dataShards[i], shardBytes.Length);
             }
-
+          
             Console.WriteLine();
 
             return dataShards;
@@ -202,15 +272,20 @@ namespace TestAPILayer.Controllers
             var transanctionValues = JSONArrayToList(binaryStringCBOR.ToJSONString());
 
             byte[] shardsCBORBytes = StringToBytes(transanctionValues[0]);
+            byte[] hmacResultCBORBytes = StringToBytes(transanctionValues[1]);
+
 
             CBORObject shardsCBOR = CBORObject.DecodeFromBytes(shardsCBORBytes);
 
-            Console.WriteLine(shardsCBOR.ToJSONString());
-           
+            //CBORObject hmacResultCBOR = CBORObject.DecodeFromBytes(hmacResultCBORBytes);
+
+            Console.WriteLine($"Shards CBOR to JSON: {shardsCBOR.ToJSONString()}");
+            //Console.WriteLine($"hmac result CBOR to JSON: {hmacResultCBOR.ToJSONString()}");
+
             // Extract the shards from the JSON string and put them in byte matrix (2D array of bytes).
             byte [][] shards = GetShardsFromJSON(shardsCBOR.ToJSONString());
             Console.WriteLine("----------------------------------------------------------------------");
-
+            
             // Get shard length (all shards are of equal length).
             int shardLength = shards[0].Length;
 
@@ -234,7 +309,7 @@ namespace TestAPILayer.Controllers
             // Replicate the other shards using Reeed-Solomom.
             var reedSolomon = new ReedSolomon.ReedSolomon(shardsPresent.Length - nParityShards, nParityShards);
             reedSolomon.DecodeMissing(shards, shardsPresent, 0, shardLength);
-
+            
             // Write the Reed-Solomon matrix of shards to a 1D array of bytes
             byte[] rebuiltDataBytes = new byte[shards.Length * shardLength];
             int offSet = 0;
@@ -242,18 +317,24 @@ namespace TestAPILayer.Controllers
             {
                 Array.Copy(shards[j], 0, rebuiltDataBytes, offSet, shardLength);
                 offSet += shardLength;
-            }           
+            }
 
             // Decode rebuilt CBOR data bytes, after stripping the padding needed for the Reed-Solomon
             // which requires that all shards have to be equal in length. 
-            CBORObject rebuiltDataCBOR = CBORObject.DecodeFromBytes(StripPadding(rebuiltDataBytes));
+            byte[] cborDataBytes = StripPadding(rebuiltDataBytes);
+            Console.WriteLine($"CBOR Data bytes: {Encoding.UTF8.GetString(cborDataBytes)}");
+
+            CBORObject rebuiltDataCBOR = CBORObject.DecodeFromBytes(cborDataBytes);
+            
             string rebuiltDataString = rebuiltDataCBOR.ToJSONString();
                                  
             Console.WriteLine($"Rebuilt Data: {rebuiltDataString}");
             Console.WriteLine();
 
-            return Ok(rebuiltDataCBOR.ToJSONString());            
-           
+            return Ok(rebuiltDataCBOR.ToJSONString());
+         
+
+            return Ok("API Layer Rsponded!");
         }
     }
 }
