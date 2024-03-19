4c6a9be8790a115a9813a9a2c8d675b0b97f223e
Dimitrios Zervas <dimitri_zervas@hotmail.com>
2024-03-19 17:28:00 +0200

    SERV-5 Added LoginRequest

---
+++ TestAPILayer/Requests/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAPILayer.Requests
+{
+    public sealed class LoginRequest : BaseRequest
+    {
+        public string encKEY { get; set; }
+
+        public static byte[] DS_PUB;
+        public static byte[] DE_PUB;
+        public static byte[] NONCE;
+
+        public static List<byte[]> WENCRYPTS { get; set; }
+        public static List<byte[]> WSIGNS { get; set; }
+    }
+}
